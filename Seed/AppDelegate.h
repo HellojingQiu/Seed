5dbe80da9b95bc8563e8f8aaffbbe7535780d7e6
OliHire-HellowJingQiu <hellowjingqiu@CongYi-JokerV.local>
2015-05-07 11:37:35 +0800

    Initial Commit

---
+++ Seed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Seed
+//
+//  Created by OliHire-HellowJingQiu on 15/5/7.
+//  Copyright (c) 2015年 OliHire-JokerV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Seed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Seed
+//
+//  Created by OliHire-HellowJingQiu on 15/5/7.
+//  Copyright (c) 2015年 OliHire-JokerV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
